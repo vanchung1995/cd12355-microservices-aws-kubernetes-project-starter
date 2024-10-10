c60ee8ee1bef5068455d757b8b0c20068acb173d
Vũ Văn Chung <vanchung1995@gmail.com>
2024-10-10 11:12:52 +0700

    Create init-eks-cluster.sh

---
+++ scripts/init-eks-cluster.sh
@@ -0,0 +1,25 @@
+# check account aws configre is true
+aws sts get-caller-identity
+
+# if not uncomment this line and fill aws credentials:
+# aws configure
+
+# create cluster eks and nodegroup after configure account
+eksctl create cluster --name workspace-cluster --region us-east-1 --nodegroup-name workspace-nodegroup --node-type t3.small --nodes 1 --nodes-min 1 --nodes-max 2
+
+# update eks to point to cluster created. It will create a file to save context point to this cluster
+aws eks --region us-east-1 update-kubeconfig --name workspace-cluster
+
+# verify current context, should show arn:aws:eks:us-west-2:411764878390902:cluster/workspace-cluster
+kubectl config current-context
+
+# view all config of kubectl
+kubectl config view
+
+# delete eks cluster
+eksctl delete cluster --name workspace-cluster --region us-east-1
+
+# get namespace of cluster
+kubectl get namespace
+
+
