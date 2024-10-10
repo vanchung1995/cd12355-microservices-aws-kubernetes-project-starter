c36accd168d3e8963684e080e5e5714172a00d5e
Vũ Văn Chung <vanchung1995@gmail.com>
2024-10-10 17:10:20 +0700

    Update helmchart-install-postgres.sh

--- scripts/helmchart-install-postgres.sh
+++ scripts/helmchart-install-postgres.sh
@@ -1,1 +1,4 @@
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm install postgres-udacity bitnami/postgresql --set postgresqlPassword=myPassword
+
 helm install postgres-udacity oci://registry-1.docker.io/bitnamicharts/postgresql
