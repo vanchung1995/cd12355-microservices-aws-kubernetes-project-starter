77e4d7be4f6fb4a137072536e64699cbe8fd1b78
Vũ Văn Chung <vanchung1995@gmail.com>
2024-10-11 11:17:37 +0700

    Update helmchart-postgres.sh

--- db/helmchart-postgres.sh
+++ db/helmchart-postgres.sh
@@ -2,7 +2,7 @@
 # helm install postgres-udacity bitnami/postgresql --set postgresqlPassword=myPassword
 
 # create configmap for sql scripts
-kubectl create configmap my-config --from-file=create_table=./1_create_tables.sql --from-file=seed_users=./2_seed_users.sql --from-file=seed_tokens=./3_seed_tokens.sql
+kubectl create configmap initdbscripts --from-file=create_table=./1_create_tables.sql --from-file=seed_users=./2_seed_users.sql --from-file=seed_tokens=./3_seed_tokens.sql
 
 # install postgress
 helm install postgres-udacity oci://registry-1.docker.io/bitnamicharts/postgresql
