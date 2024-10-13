ba3ee21806764dad5e58c5d98585a6e4ae540d44
Vũ Văn Chung <vanchung1995@gmail.com>
2024-10-13 13:20:54 +0700

    Update helmchart-postgres.sh

--- db/helmchart-postgres.sh
+++ db/helmchart-postgres.sh
@@ -8,7 +8,14 @@ kubectl get cm
 kubectl create configmap initdbscripts --from-file=create_table.sql=./1_create_tables.sql --from-file=seed_users.sql=./2_seed_users.sql --from-file=seed_tokens.sql=./3_seed_tokens.sql
 
 # install postgress
-helm install --set primary.initdb.scriptsConfigMap=initdbscripts postgres-udacity oci://registry-1.docker.io/bitnamicharts/postgresql
+# helm install --set primary.initdb.scriptsConfigMap=initdbscripts --set primary.persistence.enabled=false  postgres-udacity oci://registry-1.docker.io/bitnamicharts/postgresql
+
+helm install    --set primary.initdb.scriptsConfigMap=initdbscripts \
+                --set primary.persistence.existingClaim=postgresql-pvc \
+                --set volumePermissions.enabled=true \
+                --set primary.persistence.enabled=false \
+                --set primary.persistence.mountPath=/bitnami/postgresql/data \
+                postgres-udacity oci://registry-1.docker.io/bitnamicharts/postgresql
 
 # uninstall postgres
 helm uninstall postgres-udacity
